2d8220ffda91256dacfae1dc112f2e086f707d67
Ievgeniia Vashchenko <dn050493vem@gmail.com>
2022-06-09 15:40:42 +0300

    task done

---
+++ models/contact.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+// const codeRegexp = /^[0-9]{9}$/;
+
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const contactJoiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(10).max(14).required(),
+  favorite: Joi.bool(),
+});
+
+const favoriteJoiSchema = Joi.object({
+  favorite: Joi.bool().required(),
+});
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  contactJoiSchema,
+  favoriteJoiSchema,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,115 +1,30 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
-const { NotFound } = require("http-errors");
-const Joi = require("joi");
 
-const contactsOperations = require("../../models/contacts");
+const { validation, ctrlWrapper } = require("../../middlewares");
+const { contactJoiSchema, favoriteJoiSchema } = require("../../models/contact");
+const { contacts: ctrl } = require("../../controllers");
+
 const router = express.Router();
 
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().min(10).max(14).required(),
-});
+router.get("/", ctrlWrapper(ctrl.listContacts));
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await contactsOperations.listContacts();
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        contacts,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", ctrlWrapper(ctrl.getContactById));
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsOperations.getContactById(id);
-    if (!result) {
-      throw new NotFound(`Contact with id=${id} not found`);
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", validation(contactJoiSchema), ctrlWrapper(ctrl.addContact));
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      error.message = "missing required name field";
-      throw error;
-    }
+router.put(
+  "/:id",
+  validation(contactJoiSchema),
+  ctrlWrapper(ctrl.updateContact)
+);
 
-    const result = await contactsOperations.addContact(req.body);
-    res.status(201).json({
-      status: "success",
-      code: 201,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch(
+  "/:id/favorite",
+  validation(favoriteJoiSchema),
+  ctrlWrapper(ctrl.updateStatusContact)
+);
 
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsOperations.removeContact(id);
-    if (!result) {
-      throw new NotFound(`Contact with id=${id} not found`);
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      message: "contact deleted",
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
-    const { id } = req.params;
-    const result = await contactsOperations.updateContact(id, req.body);
-    if (!result) {
-      throw new NotFound(`Contact with id=${id} not found`);
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:id", ctrlWrapper(ctrl.removeContact));
 
 module.exports = router;
